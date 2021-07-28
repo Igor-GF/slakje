a7f3ff7d31a95dc40dda189bd560576276f01e34
Igor Desktop <igfigueiredo@outlook.com>
2021-07-28 11:49:14 +0200

    sass setup, components and pages folders created

---
+++ src/components/Background.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Background = () => {
+  return (
+    <>
+      <img src="../../public/img/bg-volendorpje_scene_01" alt="bg-image" className="bg-image" />
+    </>
+  )
+}
+
+export default Background;
---
+++ src/pages/StoryPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Background from '../components/Background';
+
+const StoryPage = () => {
+  return (
+    <main className="page-container">
+      <h3>Let's see how it goes!</h3>
+      <Background />
+      
+    </main>
+  )
+}
+
+export default StoryPage;
